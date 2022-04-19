a61bad8ced92ccedb60efef046a4d520c84ff2b1
Luiz Duarte <luiz.duarte@vittude.com>
2022-04-18 22:29:03 -0300

    fazendo com que o código seq seja apenas a main e 1 func pra convolução, facilitando na hora de transformar em paralelo

--- aplicacao.c
+++ aplicacao.c
@@ -2,33 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Aloca memoria para uma
-// matriz de dimencoes (N x M)
-unsigned char** aloca(int m, int n){
-    int i;
-    unsigned char **M;
-    M = (unsigned char**) malloc(n * sizeof(unsigned char*));
-    for(i = 0; i < n; i++){
-        M[i] = (unsigned char*) malloc(m * sizeof(unsigned char));
-    }
-    return M;
-}
-
-int convolution(int i, int j, short int mask[][3], unsigned char **image){
-    int p, q, aux;
-    int pixel_resultante = 0;
-    i--; j--;
-    aux = j;
-    for(p = 0; p < 3; p++){
-        for(q = 0; q < 3; q++){
-            pixel_resultante += image[i][j] * mask[p][q];
-            j++;
+void convolution(int n, int m, short int mask[][3], unsigned char **original, unsigned char **resultado){  
+    int aux_i, aux_j;
+    int aux;
+    int pixel_resultante;
+    int p, q; // for interno, da mascara 3x3
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            pixel_resultante = 0;
+            aux_i = i; 
+            aux_j = j;
+            aux = j;
+            for(p = 0; p < 3; p++){
+                for(q = 0; q < 3; q++){
+                    pixel_resultante += original[aux_i][aux_j] * mask[p][q];
+                    aux_j++;
+                }
+                j = aux;
+                aux_i++;
+            }
+            //por estarmos utilizando uma matriz 3x3 de gauss, após a soma das multiplicações devemos dividir por 16
+            resultado[i][j] = pixel_resultante/16;
         }
-        j = aux;
-        i++;
     }
-     //por estarmos utilizando uma matriz 3x3 de gauss, após a soma das multiplicações devemos dividir por 16
-    return pixel_resultante/16;
 }
 
 int main(int argc, char **argv){
@@ -68,11 +64,22 @@ int main(int argc, char **argv){
     //Próximos valores do cabeçalho após P5 são: numero de colunas, numero de linhas, e valor máximo.
     fscanf(imagem, "%d %d %d", &m, &n, &max) ;
 
-
     // Matriz para guardar a imagem original
     // Utilizando o método de bordas "pretas"
     unsigned char **original;
-    original = aloca(m+2, n+2);
+    int n_col = m+2;
+    int n_row = n+2;
+    original = (unsigned char**) malloc(n_row * sizeof(unsigned char*));
+    for (i=0; i<n_row; i++){
+        original[i] = (unsigned char*) malloc(n_col * sizeof(unsigned char));
+    }
+
+    // Matriz pra servir de buffer pra imagem resultado
+    unsigned char **resultado;
+    resultado = (unsigned char**) malloc(n *sizeof(unsigned char*));
+    for (i=0; i<n;i++){
+        resultado[i] = (unsigned char*) malloc(m * sizeof(unsigned char));
+    }
 
 
     // Leitura da imagem original
@@ -92,13 +99,17 @@ int main(int argc, char **argv){
     nova_imagem = fopen(nome_imagem_saida , "w");
     fprintf(nova_imagem,"P5\n%d %d\n %d\n", m, n, max);
 
-    for(i = 1; i <= n; i++){
-        for(j = 1; j <= m; j++){
-            pixel_resultante = convolution(i, j, mask, original);
 
-            fprintf(nova_imagem, "%c", (char) pixel_resultante);
+    convolution(n, m, mask, original, resultado);
+
+
+    // escrever no arquivo resultado
+    for(i = 0; i < n; i++){
+        for(j = 0; j < m; j++){
+            fprintf(nova_imagem, "%c", (char) resultado[i][j]);
         }
     }
+
 
     fclose(imagem);
     fclose(nova_imagem);
@@ -107,5 +118,11 @@ int main(int argc, char **argv){
         free(original[i]);
     }
     free(original);
+
+    for(i = 0; i<=n; i++){
+        free(resultado[i]);
+    }
+    free(resultado);
+
     return 0;
 }
